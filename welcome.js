854ea0aa9fbd6817c0c575fee489c66fe17ff322
RAYYAN-YT <rakhtar1901@gmail.com>
2026-08-04 13:59:25 +0530

    Updated welcome embed design

--- welcome.js
+++ welcome.js
@@ -18,14 +18,38 @@ module.exports = (client) => {
 
             .setColor("#FFFFFF")
 
+            .setAuthor({
+                name: member.user.username,
+                iconURL: member.user.displayAvatarURL({
+                    dynamic: true
+                })
+            })
+
+            .setThumbnail(
+                member.user.displayAvatarURL({
+                    dynamic: true,
+                    size: 1024
+                })
+            )
+
             .setDescription(`
 <:ChatGPT_Image_Jul_19_2026_04_51_:1529099145351139328> **Welcome to Team InVorex!**
 
 > ⭐ We're excited to have you as part of our community. Take a moment to explore the server, read the rules, and choose your roles. Whether you're here to compete, connect, or simply enjoy the experience, we hope you'll feel right at home.
 
-<:Notes:1508801360223670333> **__Member count__**
-**#${member.guild.memberCount}**
+<:Notes:1508801360223670333> **__Member Count__** #${member.guild.memberCount}
             `)
+
+            .setImage(
+                member.guild.bannerURL({
+                    size: 1024
+                })
+            )
+
+            .setFooter({
+                text: "InVorex Manager",
+                iconURL: member.client.user.displayAvatarURL()
+            })
 
             .setTimestamp();
 
